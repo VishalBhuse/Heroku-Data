16863248d0fa6a092d2d131a4bb7e8dccd3a84a6
VishalBhuse <vbhuse1997@gmail.com>
2022-07-03 20:41:46 +0530

    data2

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
 const items = require("./data/items");
-const tags = require("./data/tags");
 const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+const restro = require("./data/restro");
+const userinfo = require("./data/userinfo");
+const offers = require("./data/offers");
 
 module.exports = {
   items,
-  tags,
   brands,
-  itemTypes
+  restro,
+  userinfo,
+  offers
 }
 
 
