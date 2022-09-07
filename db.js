7c3af12f7f5637a0d3bfea5cf4b70f9d797e6f7e
VishalBhuse <vbhuse1997@gmail.com>
2022-09-07 23:46:32 +0530

    modesens data

--- db.js
+++ db.js
@@ -3,13 +3,39 @@ const brands = require("./data/brands");
 const restro = require("./data/restro");
 const userinfo = require("./data/userinfo");
 const offers = require("./data/offers");
+const alert = require("./data/alert");
+const babyboys = require("./data/babyboys");
+const bath = require("./data/bath");
+const beauty = require("./data/beauty");
+const homeDecor = require("./data/homeDecor");
+const like = require("./data/like");
+const men_accessories = require("./data/men_accessories");
+const men_clothing = require("./data/men_clothing");
+const men_grooming = require("./data/men_grooming");
+const post = require("./data/post");
+const recent = require("./data/recent");
+const women_bags = require("./data/women_bags");
+const women = require("./data/women");
 
 module.exports = {
   items,
   brands,
   restro,
   userinfo,
-  offers
+  offers,
+  alert,
+  babyboys,
+  bath,
+  beauty,
+  homeDecor,
+  like,
+  men_accessories,
+  men_clothing,
+  men_grooming,
+  post,
+  recent,
+  women_bags,
+  women
 }
 
 
